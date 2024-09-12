0b9d4859c1827d23c3fb805bed7cd330eb4cd6f9
Victor Alves da Costa <101835324+Victor-Alves1@users.noreply.github.com>
2024-09-12 00:39:07 +0000

    Adicionando mapa de rendimento e de eficiencia

--- abelhas.py
+++ abelhas.py
@@ -24,22 +24,21 @@ for estado in estados:
 ############################# UNION: Unindo em um unico dataframe #############################
 dataset = pd.concat(df_list)
 
+# Retirando espaço em branco a esquerda e a direita
+dataset['cidade'] = dataset['cidade'].str.strip()
 
-#############################  Transform: Tratando tabela #############################
+# Filtrando apenas estados com produção > 0
+dataset = dataset[dataset['Quantidade produzida (Toneladas)'] > 0]
+
 # Criando dataset
 series = dataset[['cidade','producao','Quantidade produzida (Toneladas)']]
 
-# Filtrando apenas estados com produção > 0
-series = series[series['Quantidade produzida (Toneladas)'] > 0]
-
-# Copiando dataset para criação do mapa
-wide_df = series
-
 # Excluindo plantas temporarias
-excluding_sugar_cane = dataset[dataset["temporaria/permanente"] == 'Temporaria']
+#excluding_sugar_cane = dataset[dataset["temporaria/permanente"] == 'Temporaria']
 
+#############################  Transform: Tratando tabela de produção #############################
 # Separando duas colunas para o mapa
-prod_per_city = excluding_sugar_cane[['cidade','Quantidade produzida (Toneladas)']]
+prod_per_city = series[['cidade','Quantidade produzida (Toneladas)']]
 
 # Somando produção das cidades
 sum_prod_per_city = prod_per_city.groupby('cidade').sum()
@@ -47,13 +46,33 @@ sum_prod_per_city = prod_per_city.groupby('cidade').sum()
 # Reindexando após soma
 sum_prod_per_city = sum_prod_per_city.reset_index()
 
-# Retirando espaço em branco a esquerda e a direita
-sum_prod_per_city['cidade'] = sum_prod_per_city['cidade'].str.strip()
-
 #series.plot.bar(x = 'cidade' , y = 'Quantidade produzida (Toneladas)', index = 'producao', stacked=True)
 
+#############################  Transform: Tratando tabela de eficiencia #############################
+# Separando as três colunas para o mapa
+somando_prod_e_area = dataset[['cidade','Quantidade produzida (Toneladas)', 'Área colhida (Hectares)']]
+
+# somando_prod_e_area
+
+
+# Somando produção das cidades
+somando_prod_e_area = somando_prod_e_area.groupby('cidade').sum()
+
+# Reindexando após soma
+somando_prod_e_area = somando_prod_e_area.reset_index()
+
+#############################  Transform: Tratando tabela de rendimento #############################
+# Separando duas colunas para o mapa
+rendimento_per_city = dataset[['cidade','Valor da produção (Mil Reais)']]
+
+# Somando produção das cidades
+sum_rendimento_per_city = rendimento_per_city.groupby('cidade').sum()
+
+# Reindexando após soma
+sum_rendimento_per_city = sum_rendimento_per_city.reset_index()
+
 ############################# CREATING: Criando gráfico de barras #############################
-fig = px.bar(wide_df, x='cidade', y='Quantidade produzida (Toneladas)', color='producao', title="Produção agricola de Pernambuco" )
+fig = px.bar(series, x='cidade', y='Quantidade produzida (Toneladas)', color='producao', title="Produção agricola de Pernambuco" )
 
 ############################# PLOTANDO: Título #############################
 st.title('Produção agrícola em Pernambuco')
@@ -62,63 +81,77 @@ st.title('Produção agrícola em Pernambuco')
 #st.sidebar.header('User Input')
 #symbol = st.sidebar.text_input('Escolha um ativo:', 'AAPL')
 
-# Subindo dados do geo_json
-# Origem dos dados https://github.com/tbrugz/geodata-br
-brazil_data = open('data/geo_data/ne_geo_json.json', 'r')
-state_geo = json.load(brazil_data)
-
-
-
-# Criando mapa
-m = folium.Map(location=(-8.36, -38.02), zoom_start=7, control_scale=True)
-
-# Populando mapa coropletico
-folium.Choropleth(
-    geo_data=state_geo,
-    #name="choropleth",
-    data=sum_prod_per_city,
-    columns=['cidade', 'Quantidade produzida (Toneladas)'],
-    key_on="feature.properties.name",
-    nan_fill_color = "white",
-    fill_color="YlGn",
-    #fill_opacity=0.4,
-    line_opacity=0.5,
-    legend_name="Produção agricola em Pernambuco",
-    highlight=True
-).add_to(m)
-
-#Adicionando a função de destaque
-estilo = lambda x: {"fillColor": "white",
-                   "color": "black",
-                   "fillOpacity": 0.001,
-                   "weight": 0.001}
-
-estilo_destaque = lambda x: {"fillColor": "darkblue",
-                            "color": "black",
-                            "fillOpacity": 0.5,
-                            "weight": 1}
-
-highlight = folium.features.GeoJson(data = state_geo,
-                                   style_function = estilo,
-                                   highlight_function = estilo_destaque,
-                                   name = "Destaque")
-
-#Adicionando caixa de texto
-folium.features.GeoJsonTooltip(fields = ["name"],
-                              aliases = ["Cidade"],
-                               labels = False,
-                              style = ("background-color: white; color: black; font-family: arial; font-size: 16px; padding: 10px;")).add_to(highlight)
-
-#Adicionando o destaque ao mapa
-m.add_child(highlight)
-
-#folium.LayerControl().add_to(m)
-
-streamlit_folium.st_folium(m)
-st.dataframe(sum_prod_per_city)
-
 ############################# PLOTING: Grafico de barras de produtividade #############################
 st.plotly_chart(fig)
 
 ############################# PLOTING: Tabela de produtividade #############################
 st.dataframe(dataset)
+
+# Subindo dados do geo_json
+# Origem dos dados https://github.com/tbrugz/geodata-br
+brazil_data = open('data/geo_data/ne_geo_json.json', 'r')
+state_geo = json.load(brazil_data)
+
+tab1, tab2, tab3 = st.tabs(["🗺️ Produção (t)", " 🎍 Eficiencia (Kg/t)", "💸 Rendimento (1.000 reais)"])
+
+with tab1:
+    st.header("🗺️ Produção (t)")
+
+    # Criando mapa
+    m1 = folium.Map(location=(-8.36, -38.02), zoom_start=7, control_scale=True)
+
+    # Populando mapa coropletico
+    folium.Choropleth(
+        geo_data=state_geo,
+        #name="choropleth",
+        data=sum_prod_per_city,
+        columns=['cidade', 'Quantidade produzida (Toneladas)'],
+        key_on="feature.properties.name",
+        nan_fill_color = "white",
+        fill_color="YlGn",
+        fill_opacity=1,
+        line_opacity=0.5,
+        legend_name="Produção agricola em Pernambuco",
+        highlight=True
+    ).add_to(m1)
+
+    #Adicionando a função de destaque
+    estilo = lambda x: {"fillColor": "white",
+                    "color": "black",
+                    "fillOpacity": 0.001,
+                    "weight": 0.001}
+
+    estilo_destaque = lambda x: {"fillColor": "darkblue",
+                                "color": "black",
+                                "fillOpacity": 0.5,
+                                "weight": 1}
+
+    highlight = folium.features.GeoJson(data = state_geo,
+                                    style_function = estilo,
+                                    highlight_function = estilo_destaque,
+                                    name = "Destaque")
+
+    #Adicionando caixa de texto
+    folium.features.GeoJsonTooltip(fields = ["name"],
+                                aliases = ["Cidade"],
+                                labels = False,
+                                style = ("background-color: white; color: black; font-family: arial; font-size: 16px; padding: 10px;")).add_to(highlight)
+
+    #Adicionando o destaque ao mapa
+    m1.add_child(highlight)
+    #folium.LayerControl().add_to(m)
+    streamlit_folium.st_folium(m1)
+    st.dataframe(sum_prod_per_city)
+
+with tab2:
+    st.header("🎍 Rendimento (Kg/t)")
+    st.dataframe(somando_prod_e_area)
+
+with tab3:
+    st.header("💸 Rendimento (1.000 reais)")
+    st.dataframe(sum_rendimento_per_city)
+
+
+
+    
+
